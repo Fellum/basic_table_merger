f6f7c4b18c1f7b53523ac2afae5c4b9cb95ea172
fellum <fkekerman@gmail.com>
2019-11-05 01:34:51 +0300

    Update for another bases

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ import sys
 from os import walk
 
 
+def preprocess_table(table):
+    if "Фамилия" not in table.columns or "Имя" not in table.columns or "Отчество" not in table.columns:
+        print("Didnt found separated name fields, loking for ФИО field")
+        if "ФИО" not in table.columns:
+            print("Didnt found ФИО column, exit")
+            exit(0)
+        tmp = table.ФИО.str.split(expand=True)
+        tmp.columns = ["Фамилия", "Имя", "Отчество", "tmp"]
+        tmp[["tmp"]] = tmp[["tmp"]].fillna('')
+        tmp[["Отчество"]] = tmp[["Отчество"]].fillna('')
+        tmp["Отчество"] = tmp[['Отчество', 'tmp']].apply(lambda x: ' '.join(x), axis=1)
+        table[["Фамилия", "Имя", "Отчество"]] = tmp[["Фамилия", "Имя", "Отчество"]]
+    table["Отчество"] = table["Отчество"].fillna('')
+    table["Отчество"] = table['Отчество'].str.strip()
+    table["Фамилия"] = table["Фамилия"].str.strip()
+    table["Имя"] = table["Имя"].str.strip()
+
+
 def main(basename, lookname):
 
     f = []
@@ -11,21 +29,27 @@ def main(basename, lookname):
 
     x1 = pd.ExcelFile(basename)
     dfb = x1.parse(x1.sheet_names[0])
-    dfb.columns = ["Фамилия", "Имя", "Отчество", "КЕК"]
+    preprocess_table(dfb)
+    if "Номер счета" not in dfb.columns:
+        print("Didnt found Номер счета column, exit")
+        exit(0)
+
+    dfb = dfb[["Фамилия", "Имя", "Отчество", "Номер счета"]]
+    print(dfb)
     res = None
     for file in f:
 
         x2 = pd.ExcelFile(lookname + "/" + file)
         dft = x2.parse(x2.sheet_names[0])
-        dft = dft.iloc[2:, 1:4]
 
-        df_obj = dft.select_dtypes(['object'])
-        dft[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
-
-        dft.columns = ["Фамилия", "Имя", "Отчество"]
-        tmp = pd.merge(dft, dfb, "inner")
+        preprocess_table(dft)
+        dft = dft[["Фамилия", "Имя", "Отчество"]]
+        print(lookname + "/" + file)
+        print(dft)
+        tmp = pd.merge(dft, dfb, "inner", on=["Фамилия", "Имя", "Отчество"])
         res = pd.concat([res, tmp])
     res.to_excel("res.xlsx", index=False, header=False)
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
